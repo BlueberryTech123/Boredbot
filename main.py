9ba241bf8d405241a949f57b0b5287f902aa11e1
BlueberryTech123 <32250628+BlueberryTech123@users.noreply.github.com>
2018-12-12 15:03:57 -0800

    first (testing) command added

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import discord
+import json
+import asyncio as asio
 
-TOKEN = 481248549900648468
+TOKEN = '481248549900648468'
+async def on_message(message):
+    if message.content.startwith(">ping"):
+        await client.send_message(message.channel, "Pong!");
+    # return message "Pong!"
+client.run(TOKEN);
