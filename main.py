1acbaea390c971e6753d8ad857b54c1a2db04b1b
BlueberryTech123 <32250628+BlueberryTech123@users.noreply.github.com>
2018-12-12 21:00:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import discord
 import json
-import asyncio as asio
+import asyncio
 
-client = discord.Client();
+client = discord.Client()
 
-TOKEN = 'NDgxMjQ4NTQ5OTAwNjQ4NDY4.DvMaVw.cqOcosS9zsuc5cKcXm8Y-KRMfbQ';
+TOKEN = 'NDgxMjQ4NTQ5OTAwNjQ4NDY4.DvMaVw.cqOcosS9zsuc5cKcXm8Y-KRMfbQ'
+@client.event
 async def on_message(message):
-    if message.content.startwith(">ping"):
-        await client.send_message(message.channel, "Pong!");
+    if message.author == client.user: return
+    if message.content.startswith(">ping"):
+        await client.send_message(message.channel, "Pong!")
     # return message "Pong!"
-client.run(TOKEN);
+client.run(TOKEN)
