cfe1844286f19dcea65feba566d6336e7353c0a0
BlueberryTech123 <32250628+BlueberryTech123@users.noreply.github.com>
2018-12-12 21:45:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ client = discord.Client()
 
 TOKEN = 'NDgxMjQ4NTQ5OTAwNjQ4NDY4.DvMaVw.cqOcosS9zsuc5cKcXm8Y-KRMfbQ'
 @client.event
+async def on_ready():
+    print("Bot online")
 async def on_message(message):
     if message.author == client.user: return
     if message.content.startswith(">ping"):
         await client.send_message(message.channel, "Pong!")
-    # return message "Pong!"
+    if message.content.startswith(">help"): # --------------------------------------- "ERROR IN CODE"
+        embed = discord.Embed(
+            title = "Help",
+            description = ">ping",
+            colour = discord.colour.blue()
+        )
+        embed.set_footer(text="Warning: bot is still WIP")
+        embed.set_author(name="User request")
+        await client.say(embed = embed)
 client.run(TOKEN)
