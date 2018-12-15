016b9895ca91f887569cc6bde9b387624467cba9
BlueberryTech123 <32250628+BlueberryTech123@users.noreply.github.com>
2018-12-14 17:20:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import asyncio
 client = discord.Client()
 
 TOKEN = 'NDgxMjQ4NTQ5OTAwNjQ4NDY4.DvMaVw.cqOcosS9zsuc5cKcXm8Y-KRMfbQ'
+
+#async def on_ready():
+#    await client.change_presence(game=discord.Game(name="with the >help command!"))
+#    print("Playing status...Done")
 @client.event
-async def on_ready():
-    await client.change_presence(game=discord.Game(name="with the >help command!"))
-    print("Playing status...Done")
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith(">ping"):
         await client.send_message(message.channel, "Pong!")
-    if message.content.startswith(">help"): # --------------------------------------- "ERROR IN CODE"
+    if message.content.startswith(">help"):
         embed = discord.Embed(
             title = "Help",
             description = "Help for commands. If you\'re looking for the items, ``>shop`` would do its job :briefcase:",
@@ -24,4 +25,5 @@ async def on_message(message):
         embed.set_author(name=embed.author.name, icon_url=embed.author.avatar_url)
         embed.add_field(name="Information", value="``>help`` ``>ping``", inline=False)
         await client.send_message(message.channel, embed = embed)
+    #await client.process_commands(message)
 client.run(TOKEN)
